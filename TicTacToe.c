b4af348234ceee03ee6419397e02271bb764fa5b
dani <rosario.dani01@gmail.com>
2026-01-05 13:53:55 -0300

    add win check!

--- TicTacToe.c
+++ TicTacToe.c
@@ -9,8 +9,10 @@ void displayBoard();
 int main()
 {
     bool isPlayer1Turn = true;
+    bool checkVictory = false;
+    int numTurns = 0;
 
-    while (true)
+    while (!checkVictory && numTurns != 9)
     {
         displayBoard();
 
@@ -33,30 +35,65 @@ int main()
         }
 
         // Convertendo char para índice da matriz
-        int choiceNum = choice - '0'; 
-        int row = (choiceNum - 1) / 3; // Cálculo da linha
-        int col = (choiceNum - 1) % 3; // Cálculo da coluna
+        int choiceNum = choice - '0';
+        int row = (choiceNum - 1) / 3; 
+        int col = (choiceNum - 1) % 3; 
 
-        // Verifica se a posição já está ocupada
-        if (grid[row][col] == 'X' || grid[row][col] == 'O') 
+        if (grid[row][col] == 'X' || grid[row][col] == 'O')
         {
             printf("Position already taken! Try again.\n");
             continue;
         }
 
+        numTurns++;
+
         if (isPlayer1Turn)
         {
-            grid[row][col] = 'X'; 
+            grid[row][col] = 'X';
         }
         else
         {
-            grid[row][col] = 'O'; 
+            grid[row][col] = 'O';
         }
 
         isPlayer1Turn = !isPlayer1Turn;
+
+        // Bloco de Verificação de Vitória
+        {
+            bool row1 = (grid[0][0] == grid[0][1]) && (grid[0][1] == grid[0][2]);
+            bool row2 = (grid[1][0] == grid[1][1]) && (grid[1][1] == grid[1][2]);
+            bool row3 = (grid[2][0] == grid[2][1]) && (grid[2][1] == grid[2][2]);
+            bool col1 = (grid[0][0] == grid[1][0]) && (grid[1][0] == grid[2][0]);
+            bool col2 = (grid[0][1] == grid[1][1]) && (grid[1][1] == grid[2][1]);
+            bool col3 = (grid[0][2] == grid[1][2]) && (grid[1][2] == grid[2][2]);
+            bool diag1 = (grid[0][0] == grid[1][1]) && (grid[1][1] == grid[2][2]);
+            bool diag2 = (grid[0][2] == grid[1][1]) && (grid[1][1] == grid[2][0]);
+            
+            checkVictory = row1 || row2 || row3 || col1 || col2 || col3 || diag1 || diag2;
+
+            if (checkVictory)
+            {
+                displayBoard();
+                if (!isPlayer1Turn) 
+                {
+                    printf("Player 1 (X) wins!\n");
+                }
+                else
+                {
+                    printf("Player 2 (O) wins!\n");
+                }
+                break;
+            }
+        }
+
+        if (numTurns == 9 && !checkVictory)
+        {
+            displayBoard();
+            printf("Tie!\n");
+        }
     }
 
-    return 0;
+    return 0; 
 }
 
 void displayBoard()
