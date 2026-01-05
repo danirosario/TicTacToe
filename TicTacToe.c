1aea58d2bbb31c1e829f5a54d08d6e23cf5120d6
dani <rosario.dani01@gmail.com>
2026-01-05 11:08:19 -0300

    Board function; add player 1 and 2;

--- TicTacToe.c
+++ TicTacToe.c
@@ -1,16 +1,42 @@
 #include <stdio.h>
+#include <stdbool.h>
 
-int main() {
-    int grid[3][3] = { {1, 2, 3}, {4, 5, 6}, {7, 8, 9} };
+int main()
+{
+    bool isPlayer1Turn = true;
 
-    for (int i = 0; i < 3 ; i++) 
-    {       
-        for (int j = 0; j < 3; j++) 
-        {  
-            printf("%d ", grid[i][j]);
+    while (true)
+    {
+        displayBoard();
+        if(isPlayer1Turn)
+        {
+            printf("Player 1's turn (X): ");
         }
-        printf("\n"); 
+        else
+        {
+            printf("Player 2's turn (O): ");
+        }
     }
 
     return 0;
 }
+
+int displayBoard()
+{
+    int grid[3][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+
+    int rows = sizeof(grid) / sizeof(grid[0]);
+    int columns = sizeof(grid[0]) / sizeof(grid[0][0]);
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            printf("| %d  ", grid[i][j]);
+        }
+        printf("|");
+        printf("\n");
+    }
+    return 0;
+}
+
